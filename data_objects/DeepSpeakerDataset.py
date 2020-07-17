3bab5d40f8075fe1f996298316f6f7a96f28e52a
czy97 <chenzhengyang117@gmail.com>
2020-07-17 12:56:46 +0800

    add dataset debug
    Signed-off-by: czy97 <chenzhengyang117@gmail.com>

--- data_objects/DeepSpeakerDataset.py
+++ data_objects/DeepSpeakerDataset.py
@@ -72,3 +72,24 @@ class DeepSpeakerDataset(data.Dataset):
     def __len__(self):
         return len(self.features)
 
+
+if __name__ == "__main__":
+    from pathlib import Path
+    import torch
+    from tqdm import tqdm
+    import sys
+
+    train_dataset = DeepSpeakerDataset(
+        Path('/mnt/lustre/sjtu/home/czy97/sid/AutoSpeech/VoxCeleb1'), 300, 'train')
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset,
+        batch_size=48,
+        num_workers=sys.argv[1],
+        pin_memory=True,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    for data, label in tqdm(train_loader):
+        pass
+
